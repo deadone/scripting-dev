c0a1de40bf0671b7518318ecd01ec84a09e8257e
deadone <45006877+deadone@users.noreply.github.com>
2020-02-28 21:17:24 -0700

    Update pidprocess.py

--- proc-py/pidprocess.py
+++ proc-py/pidprocess.py
@@ -5,6 +5,15 @@
 
 from deadlib import *
 
+class color:
+    blue = '\033[94m' # blue
+    green = '\033[92m' # pale green
+    yellow = '\033[93m' # yellow
+    red = '\033[91m' # red
+    bold = '\033[1m' # bold
+    uline = '\033[4m' #underline
+    nc ='\x1b[0m' # No Color
+
 process = LinuxProcList.proclist()
 arrows = "|-------------->"
 dashes = "----------------"
@@ -15,27 +24,27 @@ for x in process:
     if str(x) not in blacklist:
         processchildren = LinuxProcList.children(x)
         if LinuxProcList.children(x) is not None:
-            print(arrows,"PID: ",str(x).ljust(8, " "),LinuxProcList.getName(x).ljust(25, " "), end="")
+            print(color.red + color.bold + arrows,str(x).ljust(8, " "),LinuxProcList.getName(x).ljust(20, " "),color.nc, end="")
             if LinuxProcList.cmdline(x) is not None:
-                print("CMD:",LinuxProcList.cmdline(x))
+                print(LinuxProcList.cmdline(x))
             else:
                 print()
             for y in LinuxProcList.children(x):
                 blacklist.append(y)
-                print(spaces + arrows,"PID: ", y.ljust(8," "),"Name:",LinuxProcList.getName(y).ljust(15, " "), end=" ")
+                print(color.blue + color.bold + spaces + arrows, y.ljust(8," "),LinuxProcList.getName(y).ljust(20, " "),color.nc, end=" ")
                 if LinuxProcList.children(y) is not None:
                     for z in LinuxProcList.children(y):
                         blacklist.append(z)
-                        print("\n" + spaces + spaces + arrows, "PID: ", z.ljust(8, " "),"Name:",LinuxProcList.getName(y).ljust(15, " "), end="")
+                        print("\n" + color.bold + color.green + spaces + spaces + arrows,z.ljust(8, " "),LinuxProcList.getName(y).ljust(20, " "),color.nc, end="")
                         if LinuxProcList.cmdline(z) is not None:
-                            print("CMD:", LinuxProcList.cmdline(z),end="")
+                            print(LinuxProcList.cmdline(z),end="")
                 if LinuxProcList.cmdline(y) is not None:
-                    print("CMD: ",LinuxProcList.cmdline(y))
+                    print(LinuxProcList.cmdline(y))
                 else:
                     print()
         else:
-            print(arrows,"PID: ",str(x).ljust(8, " "), end="")
+            print(color.red + color.bold + arrows,str(x).ljust(8, " "),color.nc, end="")
             if LinuxProcList.cmdline(x) is not None:
-                print("CMD:",LinuxProcList.cmdline(x))
+                print(LinuxProcList.cmdline(x))
             else:
                 print()
