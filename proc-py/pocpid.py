ec32db210da89f5599e83ff7c31bd1adf2764226
deadone <45006877+deadone@users.noreply.github.com>
2020-03-07 09:24:38 -0700

    Update pocpid.py

--- proc-py/pocpid.py
+++ proc-py/pocpid.py
@@ -4,8 +4,8 @@
 # Details: proc
 
 from deadlib import *
-
+# proof of concept use of classes from deadlib
 print(linux_proc_list.proc_list())
 linux_proc_list.proc_list_plus()  ## added a better viewing option if a list wasnt desired.
-print(linux_proc_list.cmd_line(4254))
+print(linux_proc_list.cmd_line(2))
 print(linux_proc_list.children(2))
