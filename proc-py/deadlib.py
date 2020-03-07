aa177bb9e5a20338a8b2e921b8cb93a9508faa73
deadone <45006877+deadone@users.noreply.github.com>
2020-03-07 09:44:16 -0700

    Update deadlib.py

--- proc-py/deadlib.py
+++ proc-py/deadlib.py
@@ -118,6 +118,29 @@ class linux_proc_list:
         else:
             return None
 
+    def process_viewer(pid):
+        if linux_proc_list.verify_pid(pid):
+            process = linux_process(pid)
+            print()
+            print("name:".rjust(12, " "), process.name().rjust(18, " "))
+            print("state:".rjust(12, " "), process.state().rjust(18, " "))
+            print("pid:".rjust(12, " "), process.pid().rjust(18, " "))
+            print("ppid:".rjust(12, " "), process.ppid().rjust(18, " "))
+            print("rss:".rjust(12, " "), hex(int(process.rss())).rjust(18, " "))
+            print("rss_limit:".rjust(12, " "), hex(int(process.rss_limit())).rjust(18, " "))
+            print("start_code:".rjust(12, " "), hex(int(process.start_code())).rjust(18, " "))
+            print("end_code:".rjust(12, " "), hex(int(process.end_code())).rjust(18, " "))
+            print("start_stack:".rjust(12, " "), hex(int(process.stack_start())).rjust(18, " "))
+            print("start_data:".rjust(12, " "), hex(int(process.start_data())).rjust(18, " "))
+            print("end_data:".rjust(12, " "), hex(int(process.end_data())).rjust(18, " "))
+            print("start_brk:".rjust(12, " "), hex(int(process.start_brk())).rjust(18, " "))
+            print("arg_start:".rjust(12, " "), hex(int(process.arg_start())).rjust(18, " "))
+            print("arg_end:".rjust(12, " "), hex(int(process.arg_end())).rjust(18, " "))
+            print("env_start:".rjust(12, " "), hex(int(process.env_start())).rjust(18, " "))
+            print("env_end:".rjust(12, " "), hex(int(process.env_end())).rjust(18, " "))
+        else:
+            print("Process is not running.")
+
 
 # object class for processes and stat file
 class linux_process:
