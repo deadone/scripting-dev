e9a121d000cb3af51d108248a1fbe9964f596da6
deadone <45006877+deadone@users.noreply.github.com>
2020-03-07 09:23:24 -0700

    Update pidinfo.py

--- proc-py/pidinfo.py
+++ proc-py/pidinfo.py
@@ -14,47 +14,42 @@ import sys
 from deadlib import *
 
 if len(sys.argv) == 2:
-    pidStr = sys.argv[1]
+    pid = sys.argv[1]
 else:
-    pidStr = str(os.getpid())
+    pid = str(os.getpid())
 
 loop = 1
 while loop == 1:
-    dirStr = "/proc/" + pidStr + "/"
-    if os.path.isdir(dirStr):
-        statStr = dirStr + "/stat"
-        statusStr = dirStr + "/status"
-        if os.path.isfile(statStr) and os.path.isfile(statusStr):
-            process = LinuxProcess(pidStr)
-        else:
-            print('error.')
-            exit()
+    dir_string = "/proc/" + pid + "/"
+    if os.path.isdir(dir_string):
+        process = linux_process(pid)
     else:
         print("Process Not Running.")
         exit()
-    LinuxProcList.proclistPlus()
+
+    linux_proc_list.proc_list_plus()
     print()
     print("Process Viewer:")
     print("name:".rjust(12, " "), process.name().rjust(18, " "))
     print("state:".rjust(12, " "), process.state().rjust(18, " "))
     print("pid:".rjust(12, " "), process.pid().rjust(18, " "))
     print("ppid:".rjust(12, " "), process.ppid().rjust(18, " "))
-    print("rss:".rjust(12, " "), process.rss().rjust(18, " "))
-    print("rss_limit:".rjust(12, " "), process.rssLimit().rjust(18, " "))
-    print("start_code:".rjust(12, " "), process.startCode().rjust(18, " "))
-    print("end_code:".rjust(12, " "), process.endCode().rjust(18, " "))
-    print("start_stack:".rjust(12, " "), process.stackStart().rjust(18, " "))
-    print("start_data:".rjust(12, " "), process.startData().rjust(18, " "))
-    print("end_data:".rjust(12, " "), process.endData().rjust(18, " "))
-    print("start_brk:".rjust(12, " "), process.startBrk().rjust(18, " "))
-    print("arg_start:".rjust(12, " "), process.argStart().rjust(18, " "))
-    print("arg_end:".rjust(12, " "), process.argEnd().rjust(18, " "))
-    print("env_start:".rjust(12, " "), process.envStart().rjust(18, " "))
-    print("env_end:".rjust(12, " "), process.envEnd().rjust(18, " "))
+    print("rss:".rjust(12, " "), hex(int(process.rss())).rjust(18, " "))
+    print("rss_limit:".rjust(12, " "), hex(int(process.rss_limit())).rjust(18, " "))
+    print("start_code:".rjust(12, " "), hex(int(process.start_code())).rjust(18, " "))
+    print("end_code:".rjust(12, " "), hex(int(process.end_code())).rjust(18, " "))
+    print("start_stack:".rjust(12, " "), hex(int(process.stack_start())).rjust(18, " "))
+    print("start_data:".rjust(12, " "), hex(int(process.start_data())).rjust(18, " "))
+    print("end_data:".rjust(12, " "), hex(int(process.end_data())).rjust(18, " "))
+    print("start_brk:".rjust(12, " "), hex(int(process.start_brk())).rjust(18, " "))
+    print("arg_start:".rjust(12, " "), hex(int(process.arg_start())).rjust(18, " "))
+    print("arg_end:".rjust(12, " "), hex(int(process.arg_end())).rjust(18, " "))
+    print("env_start:".rjust(12, " "), hex(int(process.env_start())).rjust(18, " "))
+    print("env_end:".rjust(12, " "), hex(int(process.env_end())).rjust(18, " "))
     print()
-    redo = str(input("Do you want to select another process? [y/n]: "))
+    redo = str(input("Do you want to select another target? [y/n]: "))
     if redo == "y":
-        pidStr = str(input("Please input the PID: "))
+        pid = str(input("Please input the PID: "))
         print()
     else:
         print("Exiting.")
