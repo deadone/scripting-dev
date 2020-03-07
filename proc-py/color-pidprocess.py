54d3f05262c992027edd674035f36bbe6041b025
deadone <45006877+deadone@users.noreply.github.com>
2020-03-07 10:04:23 -0700

    Update color-pidprocess.py

--- proc-py/color-pidprocess.py
+++ proc-py/color-pidprocess.py
@@ -8,21 +8,21 @@ from deadlib import *
 def recursive_child(x, blacklist, repeat):
     arrows = "\_________ "
     spaces = "           "
-    if linux_proc_list.children(x) != None:
+    if linux_proc.children(x) != None:
         repeat += 1
         col = color_repeat(repeat)
-        for y in linux_proc_list.children(x):
+        for y in linux_proc.children(x):
             blacklist.append(y)
-            print(spaces * repeat + col + arrows + str(y).ljust(8, " ") + linux_proc_list.get_name(y).ljust(20, " "), end="")
-            if linux_proc_list.cmd_line(y) != None:
-                print(linux_proc_list.cmd_line(y) + color.nc)
+            print(spaces * repeat + col + arrows + str(y).ljust(8, " ") + linux_proc.get_name(y).ljust(20, " "), end="")
+            if linux_proc.cmd_line(y) != None:
+                print(linux_proc.cmd_line(y) + color.nc)
             else:
                 print(color.nc)
             blacklist.append(recursive_child(y, blacklist, repeat))
     return blacklist
 
 
-process = linux_proc_list.proc_list()
+process = linux_proc.proc_list()
 blacklist = []
 
 print("Dead1's PID Process Tree:")
@@ -31,9 +31,9 @@ for x in process:
     col = color_repeat(repeat)
     if str(x) not in blacklist:
         blacklist.append(x)
-        print(col + "\__________" + str(x).ljust(8, " ") + linux_proc_list.get_name(x).ljust(20, " "), end="")
-        if linux_proc_list.cmd_line(x) != None:
-            print(linux_proc_list.cmd_line(x) + color.nc)
+        print(col + "\__________" + str(x).ljust(8, " ") + linux_proc.get_name(x).ljust(20, " "), end="")
+        if linux_proc.cmd_line(x) != None:
+            print(linux_proc.cmd_line(x) + color.nc)
         else:
             print(color.nc)
         blacklist.append(recursive_child(x, blacklist, repeat))
