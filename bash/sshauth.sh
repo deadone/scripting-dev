e9078e0def407dfbc447691151581264a1902552
deadone <45006877+deadone@users.noreply.github.com>
2020-04-13 08:09:54 -0600

    Update sshauth.sh

--- bash/sshauth.sh
+++ bash/sshauth.sh
@@ -6,24 +6,25 @@
 # Username Attempts / IP Addresses
 # Writes to GODLOG everytime script is ran
 
-# Filename Output Variables to be found in logs/
+# Variables
+DIRNAME="sshlogs"    # directory used/created for output
 TRYNAME="tried.log"  # output filename for username attempts
 BANNAME="banned.log" # output filename for banned IPs
-GODLOG="godlog.log"  # updates with time script is ran
+GODLOG="godlog.log"  # chronological output file
 
-mkdir -p $PWD/logs
-cat /var/log/auth.log* | grep ssh | grep invalid | cut -b 75-150 | cut -d " " -f 3 > $PWD/logs/temp1
-sort $PWD/logs/temp1 | uniq > $PWD/logs/$TRYNAME
-zgrep 'Ban' /var/log/fail2ban.log* | cut -b 75-150 | cut -d " " -f 6 > $PWD/logs/temp2
-sort $PWD/logs/temp2 | uniq > $PWD/logs/$BANNAME
-sed -i '/^$/d' $PWD/logs/$TRYNAME
-rm $PWD/logs/temp*
-sed -i '/^$/d' $PWD/logs/$BANNAME
+mkdir -p $PWD/$DIRNAME
+cat /var/log/auth.log* | grep ssh | grep invalid | cut -b 75-150 | cut -d " " -f 3 > $PWD/$DIRNAME/temp1
+sort $PWD/$DIRNAME/temp1 | uniq > $PWD/$DIRNAME/$TRYNAME
+zgrep 'Ban' /var/log/fail2ban.log* | cut -b 75-150 | cut -d " " -f 6 > $PWD/$DIRNAME/temp2
+sort $PWD/$DIRNAME/temp2 | uniq > $PWD/$DIRNAME/$BANNAME
+rm $PWD/$DIRNAME/temp*
+sed -i '/^$/d' $PWD/$DIRNAME/$TRYNAME
+sed -i '/^$/d' $PWD/$DIRNAME/$BANNAME
 LLDATE=`date`
-USERT=`wc -w $PWD/logs/$TRYNAME | cut -d " " -f 1`
-BANNT=`wc -w $PWD/logs/$BANNAME | cut -d " " -f 1`
-echo -e "${LLDATE}\tUsernames: ${USERT}\tBanned: ${BANNT}" >> $PWD/logs/$GODLOG
+USERT=`wc -w $PWD/$DIRNAME/$TRYNAME | cut -d " " -f 1`
+BANNT=`wc -w $PWD/$DIRNAME/$BANNAME | cut -d " " -f 1`
+echo -e "${LLDATE}\tUsernames: ${USERT}\tBanned: ${BANNT}" >> $PWD/$DIRNAME/$GODLOG
 echo -e "\n *SSH Server Authentication Stats*"
 echo -e "  [ ${BANNT} ] Banned IPs\n  [ ${USERT} ] Attempted Usernames"
 echo -e " Logs Located:"
-echo -e "  ${PWD}/logs/${GODLOG}\n  ${PWD}/logs/${BANNAME}\n  ${PWD}/logs/${TRYNAME}\n"
+echo -e "  ${PWD}/$DIRNAME/${GODLOG}\n  ${PWD}/$DIRNAME/${BANNAME}\n  ${PWD}/$DIRNAME/${TRYNAME}\n"
