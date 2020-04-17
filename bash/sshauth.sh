8869eb6cf1a9287c1bcb2420a13dd6315048c10f
deadone <45006877+deadone@users.noreply.github.com>
2020-04-17 12:57:47 -0600

    Update sshauth.sh

--- bash/sshauth.sh
+++ bash/sshauth.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 # Author: dead1
-# Script to Log SSH Authorization Attempts
+# SSH Authorization Attempts
 # Parses Fail2ban.log and Auth.log
 # Requires Fail2ban to be installed
 # Username Attempts / IP Addresses
-# Writes to GODLOG everytime script is ran
 
 # Variables
-DIRNAME="sshlogs"    # directory used/created for output
-TRYNAME="tried.log"  # output filename for username attempts
-BANNAME="banned.log" # output filename for banned IPs
-GODLOG="godlog.log"  # chronological output file
-DISLOG=5             # number of logs to display
+DIRNAME="auth-ssh"  # directory used/created for output
+TRYNAME="user.log"  # output filename for username attempts
+BANNAME="ip.log"    # output filename for banned IPs
 
-clear
 mkdir -p $PWD/$DIRNAME
 zgrep 'Invalid' /var/log/auth.log* | cut -b 75-150 | cut -d " " -f 2 > $PWD/$DIRNAME/temp1
 zgrep 'Ban' /var/log/fail2ban.log* | cut -b 75-150 | cut -d " " -f 6 > $PWD/$DIRNAME/temp2
@@ -29,15 +25,5 @@ LUSRS=0
 LBANS=0
 echo -e "\n >> SSH Server Authentication Stats <<"
 echo -e "  [ ${BANNT} ] Banned IPs\n  [ ${USERT} ] Attempted Usernames"
-GODFILE="${PWD}/${DIRNAME}/${GODLOG}"
-if [ -f "$GODFILE" ]; then
-	LUSRS=`tail -n 1 $GODFILE | cut -d " " -f 9`
-	LBANS=`tail -n 1 $GODFILE | cut -d " " -f 15`
-	echo -e "  [ $(($USERT-$LUSRS)) ] New Usernames\n  [ $(($BANNT-$LBANS)) ] New IPs"
-fi
-echo -e " Logs Located:"
-echo -e "  ${PWD}/${DIRNAME}/${GODLOG}\n  ${PWD}/${DIRNAME}/${BANNAME}\n  ${PWD}/${DIRNAME}/${TRYNAME}\n"
-echo -e " Last [ ${DISLOG} ] Logs"
-echo -e "${LLDATE} [Total Usernames: ${USERT} / New: $(($USERT-$LUSRS))] [Total Banned: ${BANNT} / New: $(($BANNT-$LBANS))]" >> $PWD/$DIRNAME/$GODLOG
-cat $PWD/$DIRNAME/$GODLOG | tail -n $DISLOG
-echo -e ""
+echo -e " Logs Created:"
+echo -e "  ${PWD}/${DIRNAME}/${BANNAME}\n  ${PWD}/${DIRNAME}/${TRYNAME}\n"
