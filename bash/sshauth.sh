718f478e53ea23446b99b7fc12de1c0c0c5ff0de
deadone <45006877+deadone@users.noreply.github.com>
2020-04-17 16:45:15 -0600

    Update sshauth.sh

--- bash/sshauth.sh
+++ bash/sshauth.sh
@@ -10,7 +10,7 @@ BANNAME="ip.log"    # output filename for banned IPs (if fail2ban is enabled)
 
 echo -e " >> SSH Server Authentication Stats <<"
 if [ -f "/var/log/auth.log" ]; then
-    zgrep 'Invalid' /var/log/auth.log* | cut -b 75-150 | cut -d " " -f 2 > $PWD/temp1
+	zgrep 'Invalid' /var/log/auth.log* | cut -b 75-150 | cut -d " " -f 2 > $PWD/temp1
 	sort $PWD/temp1 | uniq > $PWD/$TRYNAME
 	sed -i '/^$/d' $PWD/$TRYNAME
 	rm $PWD/temp1
@@ -22,7 +22,7 @@ else
 fi
 
 if [ -f "/var/log/fail2ban.log" ]; then
-    zgrep 'Ban' /var/log/fail2ban.log* | cut -b 75-150 | cut -d " " -f 6 > $PWD/temp2
+	zgrep 'Ban' /var/log/fail2ban.log* | cut -b 75-150 | cut -d " " -f 6 > $PWD/temp2
 	sort $PWD/temp2 | uniq > $PWD/$BANNAME
 	sed -i '/^$/d' $PWD/$BANNAME
 	rm $PWD/temp2
