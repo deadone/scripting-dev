fb7a9798379a92a1825f9fd186ef6ef068ecdd26
deadone <45006877+deadone@users.noreply.github.com>
2020-04-17 10:09:11 -0600

    Update auto-sshauth.sh

--- bash/auto-sshauth.sh
+++ bash/auto-sshauth.sh
@@ -6,7 +6,7 @@
 # Username Attempts / IP Addresses
 # Writes to GODLOG everytime script is ran
 
-# Variables
+# Script Variables
 DIRNAME="sshlogs"    # directory used/created for output
 TRYNAME="tried.log"  # output filename for username attempts
 BANNAME="banned.log" # output filename for banned IPs
@@ -14,8 +14,36 @@ GODLOG="godlog.log"  # chronological output file
 SLPTME=1800          # seconds (3600 = 1h)
 DISLOG=10            # number of logs to display
 
+# Time Sync
+# Generates Cleaner Logging:
+# Finds the next half-hour or hour to sync the start of the script
+# change value to false to remove
+DO_SYNC=true
+
+function time_sync {
+        clear
+        local TS_START=$(date +%s)
+        local TS_INI=$(($TS_START/1800))
+        local TS_MOD=$(($TS_INI+1))
+        local TS_FIN=$(($TS_MOD*1800))
+        local TS_SLP=$(($TS_FIN-$TS_START))
+        local RIGHT_NOW=`date`
+        local NEXT_TIME=`date -d @${TS_FIN}`
+        echo -e "\n >> Time Sync <<"
+        echo -e " Current Time:\t${RIGHT_NOW}"
+        echo -e " Start Time:\t${NEXT_TIME}"
+        echo -e " Countdown:\t${TS_SLP} Seconds"
+        echo -e "\n Press Ctrl+C to Cancel"
+        sleep $TS_SLP
+}
+
+if [ "$DO_SYNC" = "true" ]; then
+        time_sync
+fi
+
 while [ 1 ]
 do
+	LLDATE=`date`
 	clear
 	mkdir -p $PWD/$DIRNAME
 	zgrep 'Invalid' /var/log/auth.log* | cut -b 75-150 | cut -d " " -f 2 > $PWD/$DIRNAME/temp1
@@ -25,7 +53,6 @@ do
 	sed -i '/^$/d' $PWD/$DIRNAME/$TRYNAME
 	sed -i '/^$/d' $PWD/$DIRNAME/$BANNAME
 	rm $PWD/$DIRNAME/temp*
-	LLDATE=`date`
 	USERT=`wc -w $PWD/$DIRNAME/$TRYNAME | cut -d " " -f 1`
 	BANNT=`wc -w $PWD/$DIRNAME/$BANNAME | cut -d " " -f 1`
 	LUSRS=0
@@ -44,6 +71,6 @@ do
 	echo -e " Last [ ${DISLOG} ] Logs"
 	echo -e "${LLDATE} [Total Usernames: ${USERT} / New: $(($USERT-$LUSRS))] [Total Banned: ${BANNT} / New: $(($BANNT-$LBANS))]" >> $PWD/$DIRNAME/$GODLOG
 	cat $PWD/$DIRNAME/$GODLOG | tail -n $DISLOG
-	echo -e "\nPress Ctrl+C to Exit"
+	echo -e "\n Press Ctrl+C to Exit"
 	sleep $SLPTME
 done
