df6e31a7f4630963dafa45289787636c729a98ac
deadone <45006877+deadone@users.noreply.github.com>
2020-04-24 22:25:42 -0600

    Update dead_map.sh

--- bash/dead_map.sh
+++ bash/dead_map.sh
@@ -7,28 +7,33 @@
 DEAD_NMAP="-sC -sV -oA"
 
 # file management
-DEAD_PROJECT="oscp" 		# engagement name
+DEAD_PROJECT="htb" 		# engagement name
 DEAD_DIR="$PWD/$DEAD_PROJECT"
 DEAD_HOSTS="${DEAD_DIR}/host-list"
 
+# colours
+CL8='\033[1;31m' #light red
+NC='\033[0m' #No color
+COL1="${CL8}>>>${NC}"
+
 # nmap ping scan & grep for IPs/hostnames
 clear
-echo -e ">>> Dead1's Nmap Automation <<<\n"
-echo -e ">>> Networks/Interfaces You Are On"
+echo -e "${COL1} Dead1's Nmap Automation <<<\n"
+echo -e "${COL1} Networks/Interfaces You Are On"
 ifconfig | grep -i "inet " | cut -d " " -f 10
-echo -e "\n>>> Select Host/Network (eg: 10.0.0.1)"
+echo -e "\n${COL1} Select Host/Network (eg: 10.0.0.1)"
 read DEAD_NET
-echo -e "\n>>> Subnet Cidr (eg: 24)"
+echo -e "\n${COL1} Subnet Cidr (eg: 24)"
 read DEAD_NOT
-echo -e "\n>>> Ping Scanning..."
+echo -e "\n${COL1} Ping Scanning..."
 echo -e ""
 mkdir -p $DEAD_DIR
 nmap -sn ${DEAD_NET}/${DEAD_NOT} | grep -i "report for" | cut -b 22-50 > $DEAD_HOSTS
-echo -e ">>> Found Hosts"
+echo -e "${COL1} Found Hosts"
 cat ${DEAD_HOSTS}
 echo -e ""
-echo -e ">>> Proceed\n1 - Scan One Host\n2 - Scan All Hosts\n3 - Exit"
-echo -e "\n>>> Enter Choice (eg: 1)"
+echo -e "${COL1} Proceed\n1 - Scan One Host\n2 - Scan All Hosts\n3 - Exit"
+echo -e "\n${COL1} Enter Choice (eg: 1)"
 PROCEED=3
 read PROCEED
 echo -e ""
@@ -36,29 +41,31 @@ echo -e ""
 if [ "$PROCEED" == "1" ]
 then
 	rm -rf $DEAD_DIR
-	echo -e ">>> Enter Host"	
+	echo -e "${COL1} Enter Host"	
 	read DEAD_HOST
 	echo -e ""
 	mkdir -p $DEAD_PROJECT
 	nmap $DEAD_NMAP $DEAD_PROJECT/$DEAD_HOST $DEAD_HOST
-	echo -e "\n>>> Output Saved\n$DEAD_DIR/$DEAD_HOST.nmap\n"
+	echo -e "\n${COL1} Output Saved\n$DEAD_DIR/$DEAD_HOST.nmap\n"
 	exit	
 fi
 
 if [ "$PROCEED" == "2" ]
 then
 	# nmap script scan on host(s)
-	echo -e ">>> Scanning ALL hosts.. This may take awhile..\n"
+	echo -e "${COL1} Scanning ALL hosts.. This may take awhile..\n"
 	while IFS= read -r DEAD_HOST
 	do
 		DEAD_HOST=`echo $DEAD_HOST | cut -d " " -f 1`
 		mkdir -p $DEAD_DIR/$DEAD_HOST
 		nmap $DEAD_NMAP $DEAD_DIR/$DEAD_HOST/$DEAD_HOST $DEAD_HOST
 	done < "$DEAD_HOSTS"
-	echo -e "\n\n>>> Hosts Scanned"
+	echo -e "\n\n${COL1} Hosts Scanned"
 	cat ${DEAD_HOSTS}
-	echo -e "\n>>> Output Files - Located at\n>>> ${DEAD_DIR}"
+	echo -e "\n${COL1} Output Files - Located at\n>${COL1} ${DEAD_DIR}"
 	ls -la $DEAD_DIR
 	exit
 fi
 rm -rf $DEAD_DIR
+echo -e "${COL1} Exiting..\n"
+exit
