ca368b2fd151f2a05d4bb0d630a046587f242628
deadone <45006877+deadone@users.noreply.github.com>
2020-04-24 21:57:23 -0600

    Update dead_map.sh

--- bash/dead_map.sh
+++ bash/dead_map.sh
@@ -14,20 +14,20 @@ DEAD_HOSTS="${DEAD_DIR}/host-list"
 # nmap ping scan & grep for IPs/hostnames
 clear
 echo -e ">>> Dead1's Nmap Automation <<<\n"
-echo -e "> Networks you are on:"
+echo -e ">>> Networks you are on:"
 ifconfig | grep -i "inet " | cut -d " " -f 10
-echo -e "\n> Select Host/Network: (eg: 10.0.0.1)"
+echo -e "\n>>> Select Host/Network: (eg: 10.0.0.1)"
 read DEAD_NET
-echo -e "\n> Subnet: (eg: 24)"
+echo -e "\n>>> Subnet: (eg: 24)"
 read DEAD_NOT
 echo -e ""
 mkdir -p $DEAD_DIR
 nmap -sn ${DEAD_NET}/${DEAD_NOT} | grep -i "report for" | cut -b 22-50 > $DEAD_HOSTS
-echo -e "> Hosts Found:"
+echo -e ">>> Hosts Found:"
 cat ${DEAD_HOSTS}
 echo -e ""
-echo -e "> How to Proceed:\n1 - Scan One Host\n2 - Scan All Hosts\n3 - Exit"
-echo -e "\n> Enter Choice: (eg: 1)"
+echo -e ">>> How to Proceed:\n1 - Scan One Host\n2 - Scan All Hosts\n3 - Exit"
+echo -e "\n>>> Enter Choice: (eg: 1)"
 PROCEED=3
 read PROCEED
 echo -e ""
@@ -35,12 +35,12 @@ echo -e ""
 if [ "$PROCEED" == "1" ]
 then
 	rm -rf $DEAD_DIR
-	echo -e "> Enter Host:"	
+	echo -e ">>> Enter Host:"	
 	read DEAD_HOST
 	echo -e ""
-	mkdir -p $PWD/dead_output
-	nmap $DEAD_NMAP $PWD/dead_output/$DEAD_HOST $DEAD_HOST
-	echo -e "\n> Output Saved:\n$PWD/dead_output/$DEAD_HOST.nmap\n"
+	mkdir -p dead_output
+	nmap $DEAD_NMAP dead_output/$DEAD_HOST $DEAD_HOST
+	echo -e "\n>>> Output Saved:\n$PWD/dead_output/$DEAD_HOST.nmap\n"
 	exit	
 fi
 
@@ -53,9 +53,11 @@ then
 		mkdir -p $DEAD_DIR/$DEAD_HOST
 		nmap $DEAD_NMAP $DEAD_DIR/$DEAD_HOST/$DEAD_HOST $DEAD_HOST
 	done < "$DEAD_HOSTS"
-	echo -e "\n\n> Hosts Scanned:"
+	echo -e "\n\n>>> Hosts Scanned:"
 	cat ${DEAD_HOSTS}
+	echo -e "\n>>> Output Files\n>>> Located at ${DEAD_DIR}"
+	ls -la $DEAD_DIR
 	exit
 fi
-echo -e "\n.. Exiting\n"
+echo -e ".. Exiting\n"
 exit
