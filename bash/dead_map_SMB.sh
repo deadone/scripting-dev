0e59d681250db8b42473b7249f9ca29b099ca99d
deadone <45006877+deadone@users.noreply.github.com>
2020-05-05 15:26:41 -0600

    Update dead_map_SMB.sh

--- bash/dead_map_SMB.sh
+++ bash/dead_map_SMB.sh
@@ -29,29 +29,34 @@ NICED="${DGRN}[*]${NC}"
 
 scan_vuln() {
 	echo -e "${NICED} Starting Nmap Network Enumeration Scan - Please be patient."
-	nmap -p${NMAP_PORT} -Pn ${NET_TO_SCAN} > enum.scan
-	cat enum.scan | grep -B 4 open | grep for | cut -b 22-40 > ${NMAP_PORT}.scan
-	HOST_WC=`cat ${NMAP_PORT}.scan | wc | cut -d " " -f 6`
-	rm enum.scan
-	echo -e "${NICED} ${DRED}${HOST_WC}${NC} Hosts Running Port ${DYEL}${NMAP_PORT}${NC} on ${DYEL}${NET_TO_SCAN}${NC}:"
-	cat ${NMAP_PORT}.scan
+	nmap -p${NMAP_PORT} -Pn ${NET_TO_SCAN} > /tmp/enum.scan
+	cat /tmp/enum.scan | grep -B 4 open | grep -i "for" | cut -b 22-40 > /tmp/${NMAP_PORT}.scan
+	HOST_WC=`cat /tmp/${NMAP_PORT}.scan | wc | cut -d " " -f 6`
+	rm /tmp/enum.scan
+	if [ "$HOST_WC" == "" ]
+	then
+		echo -e "${DRED}No Hosts Found! Check the Network.${NC}"
+		rm /tmp/${NMAP_PORT}.scan
+		exit
+	fi
+	echo -e "${NICED} ${DRED}${HOST_WC}${NC} Hosts Running Port ${DYEL}${NMAP_PORT}${NC}:"
+	cat /tmp/${NMAP_PORT}.scan
 	echo -e "\n${NICED} Starting ${DRED}${NMAP_SCRIPT}${NC} Scan Against Them - Please be patient."	
-
-	nmap -Pn -p${NMAP_PORT} --open --max-hostgroup 3 --script ${NMAP_SCRIPT} -iL ${NMAP_PORT}.scan > vuln.out
-	rm ${NMAP_PORT}.scan
-	cat vuln.out | grep -i -B 8 -A 4 vulnerable >  ${FIND_VULN}-vuln.out
-	rm vuln.out
+	nmap -Pn -p${NMAP_PORT} --open --max-hostgroup 3 --script ${NMAP_SCRIPT} -iL /tmp/${NMAP_PORT}.scan > /tmp/vuln.out
+	rm /tmp/${NMAP_PORT}.scan
+	cat /tmp/vuln.out | grep -i -B 8 -A 4 vulnerable > /tmp/${FIND_VULN}-vuln.out
+	rm /tmp/vuln.out
 	echo -e "${NICED} Vulnerable Targets:${DRED}"
-	cat ${FIND_VULN}-vuln.out | grep for | cut -b 22-40
+	cat /tmp/${FIND_VULN}-vuln.out | grep -i "for" | cut -b 22-40
 	## comment following (1) lines out to create log
-	rm ${FIND_VULN}-vuln.out
+	rm /tmp/${FIND_VULN}-vuln.out
 }
 
 LOG_DATE=`date +"%D - %T"`
-echo -e "${DGRN}>>> Dead1's OSCP Network Vuln Scan <<<"
-echo -e "${DBLU}${LOG_DATE}${NC}"
+echo -e "${DGRN}>>> Dead1s OSCP Network Vuln Scan <<<"
+echo -e "${DBLU}${LOG_DATE}${NC} - ${DYEL}${NET_TO_SCAN}"
 scan_vuln
 ## uncomment line below for log
-#echo -e "${NC}Log Created: ${DGRN}${PWD}/${FIND_VULN}-vuln.out${NC}\n"
+#echo -e "${NC}Log Created: ${DGRN}/tmp//${FIND_VULN}-vuln.out${NC}\n"
 echo -e "${NC}"
 exit
