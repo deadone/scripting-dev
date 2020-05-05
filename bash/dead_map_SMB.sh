df97a5f045ec19b4ed9a974886357a0870f88109
deadone <45006877+deadone@users.noreply.github.com>
2020-05-05 12:19:28 -0600

    Create dead_map_SMB.sh

---
+++ bash/dead_map_SMB.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# author: dead1
+# quick script to scan network machines for different SMB vulns
+
+### SMB Vuln Scripts
+# smb-vuln-conficker.nse
+# smb-vuln-cve2009-3103.nse
+# smb-vuln-cve-2017-7494.nse
+# smb-vuln-ms17-010.nse
+# smb-vuln-ms06-025.nse
+# smb-vuln-ms07-029.nse
+# smb-vuln-ms08-067.nse
+# smb-vuln-ms10-054.nse
+# smb-vuln-ms10-061.nse
+
+# input network to scan here
+NET_TO_SCAN="10.11.1.10/24"			# network array to scan
+FIND_VULN="ms17-010" 				# name of vuln
+NMAP_SCRIPT="smb-vuln-ms17-010"		# nmap script to run
+NMAP_PORT=445						# port scan runs on
+
+# colours
+DRED='\033[1;31m' #light red
+DBLU='\033[0;34m' #light blue
+DYEL='\033[1;33m' #yellow
+DGRN='\033[0;32m' #green
+NC='\033[0m' #color off
+NICED="${DGRN}[*]${NC}"
+
+scan_vuln() {
+	echo -e "${NICED} Starting Nmap Network Enumeration Scan - Please be patient."
+	nmap -p${NMAP_PORT} -Pn ${NET_TO_SCAN} > enum.scan
+	echo -e "${NICED} Starting ${DRED}${FIND_VULN}${NC} Scan on Enumerated Targets"
+	cat enum.scan | grep -B 4 open | grep for | cut -b 22-40 > ${NMAP_PORT}.scan
+	nmap -Pn -p${NMAP_PORT} --open --max-hostgroup 3 --script ${NMAP_SCRIPT} -iL ${NMAP_PORT}.scan > vuln.out
+	cat vuln.out | grep -i -B 8 -A 4 vulnerable >  ${FIND_VULN}-vuln.out
+	echo -e "${NICED} Vulnerable Targets:${DRED}"
+	cat ${FIND_VULN}-vuln.out | grep for | cut -b 22-40
+	rm enum.scan
+	rm ${NMAP_PORT}.scan
+	rm vuln.out
+}
+
+LOG_DATE=`date +"%D - %T"`
+echo -e "${DGRN}>>> Dead1's VULN OSCP Network Nmap Scan <<<"
+echo -e "Scanning for: ${DRED}${FIND_VULN}${NC}"
+echo -e "${DBLU}${LOG_DATE}${NC}"
+scan_vuln
+echo -e "${NC}Log Created: ${DGRN}${PWD}/${FIND_VULN}-vuln.out${NC}\n"
+exit
